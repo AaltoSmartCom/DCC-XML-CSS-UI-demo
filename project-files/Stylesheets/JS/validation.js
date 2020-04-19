275528ce712723c7fb0a79f43a9379215b172a0b
JaanTaponen <jaan.taponen@hotmail.com>
2020-04-19 11:32:19 +0300

    added filename to validation

--- project-files/Stylesheets/JS/validation.js
+++ project-files/Stylesheets/JS/validation.js
@@ -3,17 +3,17 @@ const imagePath = "../Stylesheets/images/";
 
 async function validate() {
   var path = window.location.pathname;
-  var fullpath = "";
+  var filename = "";
   if (path.endsWith(".xml")) {
-    fullpath = path.match("^\/.*\.xml").toString();
+    filename = path.split("/").pop().toString();
   } else {
-    fullpath = "../../XML/DCC_BX-10504-CAL.xml";
+    //fullpath = "../../XML/DCC_BX-10504-CAL.xml";
   }
-  const page = await (await fetch(fullpath)).text();
+  const page = await (await fetch(path)).text();
   document.getElementById("validationImage").src = imagePath + "uncertain.png"
   let base64encode = utoa(page);
   
-  let data = JSON.parse("{\r\n  \"signedDocument\" : {\r\n    \"bytes\" : \""+ base64encode +"\",\r\n    \"digestAlgorithm\" : null,\r\n    \"name\" : \"DCC_BX-10504-CAL-signed-xades-baseline-t.xml\"\r\n  },\r\n  \"policy\" : null,\r\n  \"signatureId\" : null\r\n}");
+  let data = JSON.parse("{\r\n  \"signedDocument\" : {\r\n    \"bytes\" : \""+ base64encode +"\",\r\n    \"digestAlgorithm\" : null,\r\n    \"name\" : \""+ filename +"\"\r\n  },\r\n  \"policy\" : null,\r\n  \"signatureId\" : null\r\n}");
   let validationReport = await (await fetch("http://localhost:8080/services/rest/validation/validateSignature", {
     method: 'POST',
     mode: 'cors',
@@ -26,7 +26,7 @@ async function validate() {
     referrerPolicy: 'no-referrer',
     body: JSON.stringify(data)
   })).text();
-  //console.log(JSON.parse(validationReport));
+  console.log(JSON.parse(validationReport));
   var report = checkReport(JSON.parse(validationReport));
   if (report[0] && report[1]) {
     document.getElementById("validationImage").src = imagePath + "digital_signature_signed.png";
